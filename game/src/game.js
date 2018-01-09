59816bababdeacd93fdacf804adb9b5b2bf4a2a6
DoctorVWA <vi4-weigert@hotmail.com>
2018-01-09 19:21:51 -0200

    USE THE FUCKING WHS, NOT THE FUCKING THREE

--- game/src/game.js
+++ game/src/game.js
@@ -1,5 +1,5 @@
 import { app } from 'core/app';
-import { AmbientLight, Plane } from 'whs';
+import { AmbientLight, Plane, Importer } from 'whs';
 import { addMultipleTo } from 'core/helpers';
 import {resolve} from 'path';
 
@@ -7,10 +7,6 @@ const scene = new THREE.Scene();
 
 const log = (...args) => console.log(args);
 const loader = new THREE.FBXLoader(new THREE.LoadingManager());
-
-loader.load(resolve(__dirname, 'game/src/assets/mais1test (1).fbx'), (obj) => {
-  scene.add(obj);
-}, log,log);
 
 addMultipleTo(app,
   new AmbientLight({
@@ -26,7 +22,17 @@ addMultipleTo(app,
       x: -Math.PI / 2,
     },
   }),
+  new Importer({
+    loader: new THREE.FBXLoader(new THREE.LoadingManager()),
+    url: resolve(__dirname, 'game/src/assets/esse_vai.fbx'),
+
+    parser(geometry, material) {
+      log(geometry);
+      log(material);
+      return new THREE.Mesh(geometry, material);
+    },
+    position: [0, 100, 0],
+  }),
 );
-app.setScene(scene);
 
 app.start();
