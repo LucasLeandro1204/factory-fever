aa59b79a721b2ba8436f1f436ae4b11a8f2d3037
DoctorVWA <vi4-weigert@hotmail.com>
2018-01-14 15:51:49 -0200

    isn't working, layers

--- game/src/game.js
+++ game/src/game.js
@@ -1,5 +1,6 @@
 import App from 'core/App';
 import * as Pixi from 'pixi.js';
+import * as display from 'pixi-display';
 import { loader } from 'pixi.js';
 import IsometricBlock from 'core/IsometricBlock';
 
@@ -25,9 +26,19 @@ const three = Three.setup('three', [
 
 loader.add('ground', 'src/assets/sprites/ground.png');
 
+console.log(display);
+
 const container = new Pixi.Container();
+const groundGroup = new display.Group(0, (sprite) => {
+  sprite.zOrder = -sprite.y;
+});
+const thingsGroup = new display.Group(1, (sprite) => {
+  sprite.zOrder = +sprite.y;
+});
 container.rotation = 62.05;
 app.stage.addChild(container);
+app.stage.addChild(new display.Layer(groundGroup));
+app.stage.addChild(new display.Layer(thingsGroup));
 
 loader.load((loader, resources) => {
   for (let y = 0; y < 25; y++) {
@@ -38,6 +49,7 @@ loader.load((loader, resources) => {
       const d = 100;
       ground.x = x * d;
       ground.y = y * d;
+      ground.parentGroup = groundGroup;
 
       if (Math.floor(Math.random() * 100)-1 <= 25) {
         const three2 = new Three();
@@ -47,6 +59,7 @@ loader.load((loader, resources) => {
         three2._sprite.y = -141;
         three2._sprite.anchor.set(.5);
         three2._sprite.scale.set(6);
+        three2.parentGroup = thingsGroup;
       }
       container.addChild(ground);
     }
