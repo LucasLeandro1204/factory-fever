a3c4ab13b2ed246f271bda5f4dbfc54e216d6084
DoctorVWA <vi4-weigert@hotmail.com>
2018-01-15 08:46:14 -0200

    something like that

--- game/src/core/Config.js
+++ game/src/core/Config.js
@@ -1,4 +1,5 @@
 export default {
   MOVE_SPEED: 50,
   ZOOM_SPEED: 0.1,
+  WORLD_SIZE: [25, 25],
 };
--- game/src/game.js
+++ game/src/game.js
@@ -2,6 +2,7 @@ import App from 'core/App';
 import Three from 'items/Three';
 import * as Pixi from 'pixi.js';
 import { loader } from 'pixi.js';
+import Config from 'core/Config';
 
 const app = new App({
   width: window.innerWidth,
@@ -12,11 +13,38 @@ loader.add('ground', 'src/assets/sprites/ground.png');
 
 const container = new Pixi.Container();
 container.rotation = 62.05;
+container.interactive = true;
 app.stage.addChild(container);
 
+const onDragStart = (event) => {
+  this.data = event.data;
+  this.dragging = true;
+};
+const onDragEnd = () => {
+  this.dragging = false;
+  this.data = null;
+};
+const onDragMove = () => {
+  if (this.dragging) {
+
+    app.stage.pivot.y = -this.data.global.y;
+    app.stage.pivot.x = -this.data.global.x;
+  }
+};
+
+container
+  .on('mousedown', onDragStart)
+  .on('touchstart', onDragStart)
+  .on('mouseup', onDragEnd)
+  .on('mouseupoutside', onDragEnd)
+  .on('touchend', onDragEnd)
+  .on('touchendoutside', onDragEnd)
+  .on('mousemove', onDragMove)
+  .on('touchmove', onDragMove);
+
 loader.load((loader, resources) => {
-  for (let y = 0; y < 25; y++) {
-    for (let x = 25; x > 0; x--) {
+  for (let y = 0; y < Config.WORLD_SIZE[1]; y++) {
+    for (let x = Config.WORLD_SIZE[0]; x > 0; x--) {
       const ground = new Pixi.Sprite(resources.ground.texture);
       ground.scale.set(.2);
       ground.anchor.set(.5);
